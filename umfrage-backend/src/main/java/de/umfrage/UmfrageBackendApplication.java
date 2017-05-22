2459e25650a96b8751bf2123d265e2ca9bcdd273
Notronic <florianmueller86@web.de>
2017-05-22 20:21:21 +0200

    Initiale Projekterstellung

---
+++ umfrage-backend/src/main/java/de/umfrage/UmfrageBackendApplication.java
@@ -0,0 +1,12 @@
+package de.umfrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmfrageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmfrageBackendApplication.class, args);
+	}
+}
---
+++ umfrage-frontend/src/main/java/de/umfrage/UmfrageFrontendApplication.java
@@ -0,0 +1,12 @@
+package de.umfrage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmfrageFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmfrageFrontendApplication.class, args);
+	}
+}
